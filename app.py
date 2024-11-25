45b857c3854ef733a3cf921f459b9ac6e6bfb419
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-11-25 11:07:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
